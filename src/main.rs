713d2db9f25a05fa7e0951a4ee4a412c193fdb24
sky-86 <sky86one@gmail.com>
2022-05-06 14:54:31 -0500

    Refactored a lot, working on better visuals

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,90 @@
 #![warn(clippy::all, clippy::pedantic)]
 use bracket_lib::prelude::*;
 use rand::prelude::*;
+use std::fmt;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 80;
-const FRAME_DURATION: f32 = 75.0;
+const FRAME_DURATION: f32 = 150.0;
 const VECTOR_SIZE: u16 = 80;
 
-struct Data {
-    vector: Vec<u16>,
-    current: usize,
-    smallest: usize,
-    i: usize,
-    j: usize,
+fn create_random_vector() -> Vec<u16> {
+    let mut vec: Vec<u16> = (1..=VECTOR_SIZE).collect();
+    vec.shuffle(&mut thread_rng());
+    vec
 }
 
-impl Data {
-    fn new() -> Self {
-        let mut vec: Vec<u16> = (1..=VECTOR_SIZE).collect();
-        vec.shuffle(&mut thread_rng());
+struct SelectionSort {
+    vector: Vec<u16>,
+    i: usize,
+    j: usize,
+    smallest: usize,
+}
 
-        Data {
-            vector: vec,
-            current: 0,
-            smallest: 0,
+impl SelectionSort {
+    fn new() -> Self {
+        SelectionSort {
+            vector: create_random_vector(),
             i: 0,
             j: 0,
+            smallest: 0,
         }
     }
 
-    fn render(&mut self, ctx: &mut BTerm) {
+    fn render(&self, ctx: &mut BTerm) {
         for (i, n) in self.vector.iter().enumerate() {
             for y in 0..*n {
                 if i == self.smallest {
                     ctx.set(i, y, RED, BLACK, to_cp437('#'));
-                } else if i == self.current {
+                } else if i == self.j {
+                    ctx.set(i, y, GREEN, BLACK, to_cp437('#'));
+                } else if i < self.i {
+                    ctx.set(i, y, YELLOW, BLACK, to_cp437('#'));
+                } else {
+                    ctx.set(i, y, LIGHT_BLUE, BLACK, to_cp437('#'));
+                }
+            }
+        }
+    }
+
+    fn sort(&mut self) {
+        if self.j < VECTOR_SIZE as usize {
+            if self.vector[self.j] < self.vector[self.smallest] {
+                self.smallest = self.j;
+            }
+            self.j += 1;
+        } else if self.i < VECTOR_SIZE as usize {
+            self.vector.swap(self.i, self.smallest);
+            self.i += 1;
+            self.j = self.i;
+            self.smallest = self.i;
+        }
+    }
+}
+
+struct BubbleSort {
+    vector: Vec<u16>,
+    i: usize,
+    j: usize,
+    smallest: usize,
+}
+
+impl BubbleSort {
+    fn new() -> Self {
+        BubbleSort {
+            vector: create_random_vector(),
+            i: 0,
+            j: 0,
+            smallest: 0,
+        }
+    }
+
+    fn render(&self, ctx: &mut BTerm) {
+        for (i, n) in self.vector.iter().enumerate() {
+            for y in 0..*n {
+                if i == self.smallest {
+                    ctx.set(i, y, RED, BLACK, to_cp437('#'));
+                } else if i == self.i {
                     ctx.set(i, y, GREEN, BLACK, to_cp437('#'));
                 } else {
                     ctx.set(i, y, YELLOW, BLACK, to_cp437('#'));
@@ -42,47 +92,54 @@ impl Data {
             }
         }
     }
-
-    fn sort(&mut self, algo: &str) {
-        match algo {
-            "selection" => self.selection_sort(),
-            "bubble" => self.bubble_sort(),
-            "insert" => self.insertion_sort(),
-            "gnome" => self.gnome_sort(),
-            _ => println!("Error, sorting algo"),
-        }
-    }
-
-    fn selection_sort(&mut self) {
-        let mut min = self.current;
-        for j in self.current..self.vector.len() {
-            if self.vector[j] < self.vector[min] {
-                min = j;
-            }
-        }
-
-        self.vector.swap(self.current, min);
-        self.smallest = min;
-        self.current += 1;
-    }
-
-    fn bubble_sort(&mut self) {
+    fn sort(&mut self) {
         let range = self.vector.len();
-        for j in 0..range - self.current - 1 {
+        for j in 0..range - self.i - 1 {
             if self.vector[j] > self.vector[j + 1] {
                 self.vector.swap(j, j + 1);
             }
         }
-        self.current += 1;
+        self.i += 1;
+    }
+}
+
+struct InsertionSort {
+    vector: Vec<u16>,
+    i: usize,
+    j: usize,
+    smallest: usize,
+}
+
+impl InsertionSort {
+    fn new() -> Self {
+        InsertionSort {
+            vector: create_random_vector(),
+            i: 0,
+            j: 0,
+            smallest: 0,
+        }
     }
 
-    fn insertion_sort(&mut self) {
-        if self.current == 0 {
-            self.current = 1;
+    fn render(&self, ctx: &mut BTerm) {
+        for (i, n) in self.vector.iter().enumerate() {
+            for y in 0..*n {
+                if i == self.smallest {
+                    ctx.set(i, y, RED, BLACK, to_cp437('#'));
+                } else if i == self.i {
+                    ctx.set(i, y, GREEN, BLACK, to_cp437('#'));
+                } else {
+                    ctx.set(i, y, YELLOW, BLACK, to_cp437('#'));
+                }
+            }
+        }
+    }
+    fn sort(&mut self) {
+        if self.i == 0 {
+            self.i = 1;
         }
 
-        let key = self.vector[self.current];
-        let mut j = self.current - 1;
+        let key = self.vector[self.i];
+        let mut j = self.i - 1;
 
         while self.vector[j] > key {
             self.vector.swap(j, j + 1);
@@ -91,18 +148,93 @@ impl Data {
             }
             j -= 1;
         }
-        self.current += 1;
+        self.i += 1;
+    }
+}
+
+struct GnomeSort {
+    vector: Vec<u16>,
+    i: usize,
+    j: usize,
+    smallest: usize,
+}
+
+impl GnomeSort {
+    fn new() -> Self {
+        GnomeSort {
+            vector: create_random_vector(),
+            i: 0,
+            j: 0,
+            smallest: 0,
+        }
     }
 
-    fn gnome_sort(&mut self) {
-        if self.current == 0 {
-            self.current += 1;
+    fn render(&self, ctx: &mut BTerm) {
+        for (i, n) in self.vector.iter().enumerate() {
+            for y in 0..*n {
+                if i == self.smallest {
+                    ctx.set(i, y, RED, BLACK, to_cp437('#'));
+                } else if i == self.i {
+                    ctx.set(i, y, GREEN, BLACK, to_cp437('#'));
+                } else {
+                    ctx.set(i, y, YELLOW, BLACK, to_cp437('#'));
+                }
+            }
         }
-        if self.vector[self.current] >= self.vector[self.current - 1] {
-            self.current += 1;
+    }
+
+    fn sort(&mut self) {
+        if self.i == 0 {
+            self.i += 1;
+        }
+        if self.vector[self.i] >= self.vector[self.i - 1] {
+            self.i += 1;
         } else {
-            self.vector.swap(self.current, self.current - 1);
-            self.current -= 1;
+            self.vector.swap(self.i, self.i - 1);
+            self.i -= 1;
+        }
+    }
+}
+
+enum Algorithm {
+    Selection(SelectionSort),
+    Bubble(BubbleSort),
+    Insertion(InsertionSort),
+    Gnome(GnomeSort),
+}
+
+impl Algorithm {
+    fn render(&self, ctx: &mut BTerm) {
+        match self {
+            Algorithm::Selection(s) => s.render(ctx),
+            Algorithm::Bubble(s) => s.render(ctx),
+            Algorithm::Insertion(s) => s.render(ctx),
+            Algorithm::Gnome(s) => s.render(ctx),
+        }
+    }
+
+    fn sort(&mut self, ctx: &mut BTerm) {
+        match self {
+            Algorithm::Selection(s) => {
+                for _i in 0..10 {
+                    s.sort();
+                    s.render(ctx);
+                }
+            }
+            Algorithm::Bubble(s) => s.sort(),
+            Algorithm::Insertion(s) => s.sort(),
+            Algorithm::Gnome(s) => s.sort(),
+        }
+    }
+}
+
+impl fmt::Display for Algorithm {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Algorithm::Selection(_) => write!(f, "selection sort"),
+            Algorithm::Bubble(_) => write!(f, "bubble sort"),
+            Algorithm::Insertion(_) => write!(f, "insertion sort"),
+            Algorithm::Gnome(_) => write!(f, "gnome sort"),
         }
     }
 }
@@ -110,21 +242,19 @@ impl Data {
 // stores current state
 struct State {
     frame_time: f32,
-    data: Data,
-    algorithm: String,
+    algorithm: Algorithm,
 }
 
 impl State {
     fn new() -> Self {
         State {
             frame_time: 0.0,
-            data: Data::new(),
-            algorithm: String::from("selection"),
+            algorithm: Algorithm::Selection(SelectionSort::new()),
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        ctx.cls_bg(NAVY);
+        ctx.cls_bg(BLACK);
 
         // limits framrate
         self.frame_time += ctx.frame_time_ms;
@@ -132,33 +262,21 @@ impl State {
             self.frame_time = 0.0;
         }
 
-        if let Some(VirtualKeyCode::Key1) = ctx.key {
-            self.algorithm = String::from("selection");
-        } else if let Some(VirtualKeyCode::Key2) = ctx.key {
-            self.algorithm = String::from("bubble");
-        } else if let Some(VirtualKeyCode::Key3) = ctx.key {
-            self.algorithm = String::from("insert");
-        } else if let Some(VirtualKeyCode::Key4) = ctx.key {
-            self.algorithm = String::from("gnome");
-        }
-
-        if let Some(VirtualKeyCode::Space) = ctx.key {
-            if self.data.current < VECTOR_SIZE as usize {
-                self.data.sort(&self.algorithm);
-            }
-        }
-
-        if let Some(VirtualKeyCode::R) = ctx.key {
-            self.restart();
+        match ctx.key {
+            Some(VirtualKeyCode::Key1) => println!("pressed 1"),
+            Some(VirtualKeyCode::Space) => self.algorithm.sort(ctx),
+            Some(VirtualKeyCode::R) => self.restart(),
+            Some(_) => println!("Unbinded"),
+            None => (),
         }
 
         ctx.print(0, SCREEN_HEIGHT - 2, &self.algorithm);
-        self.data.render(ctx);
+        self.algorithm.render(ctx);
     }
 
     fn restart(&mut self) {
         self.frame_time = 0.0;
-        self.data = Data::new();
+        //self.data = Data::new();
     }
 }
 
