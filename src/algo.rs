3d6f673cb8867bac31f84e4c7baf99fd92969127
sky-86 <sky86one@gmail.com>
2022-05-08 13:49:57 -0500

    Implemented traits, now algorithms share common functions

---
+++ src/algo.rs
@@ -0,0 +1,126 @@
+use bracket_lib::prelude::*;
+use rand::prelude::*;
+
+const VECTOR_SIZE: u16 = 80;
+
+fn create_random_vector() -> Vec<u16> {
+    let mut vec: Vec<u16> = (1..=VECTOR_SIZE).collect();
+    vec.shuffle(&mut thread_rng());
+    vec
+}
+
+// The shared functionality of the different algorithms
+pub trait Algorithm {
+    fn render(&self, ctx: &mut BTerm);
+    fn sort(&mut self);
+    fn reset(&mut self);
+}
+
+pub struct SelectionSort {
+    vector: Vec<u16>,
+    i: usize,
+    j: usize,
+    smallest: usize,
+}
+
+impl SelectionSort {
+    pub fn new() -> Self {
+        SelectionSort {
+            vector: create_random_vector(),
+            i: 0,
+            j: 0,
+            smallest: 0,
+        }
+    }
+}
+
+// implement the shared functions
+impl Algorithm for SelectionSort {
+    fn render(&self, ctx: &mut BTerm) {
+        for (i, n) in self.vector.iter().enumerate() {
+            for y in 0..*n {
+                if i == self.smallest {
+                    ctx.set(i, y, RED, BLACK, to_cp437('#'));
+                } else if i == self.j {
+                    ctx.set(i, y, GREEN, BLACK, to_cp437('#'));
+                } else if i < self.i {
+                    ctx.set(i, y, YELLOW, BLACK, to_cp437('#'));
+                } else {
+                    ctx.set(i, y, LIGHT_BLUE, BLACK, to_cp437('#'));
+                }
+            }
+        }
+    }
+
+    // no loops, should sort one step at a time
+    fn sort(&mut self) {
+        if self.j < VECTOR_SIZE as usize {
+            if self.vector[self.j] < self.vector[self.smallest] {
+                self.smallest = self.j;
+            }
+            self.j += 1;
+        } else if self.i < VECTOR_SIZE as usize {
+            self.vector.swap(self.i, self.smallest);
+            self.i += 1;
+            self.j = self.i;
+            self.smallest = self.i;
+        }
+    }
+
+    fn reset(&mut self) {
+        self.vector = create_random_vector();
+        self.i = 0;
+        self.j = 0;
+        self.smallest = 0;
+    }
+}
+
+pub struct BubbleSort {
+    vector: Vec<u16>,
+    i: usize,
+    j: usize,
+}
+
+impl BubbleSort {
+    pub fn new() -> Self {
+        BubbleSort {
+            vector: create_random_vector(),
+            i: 0,
+            j: 0,
+        }
+    }
+}
+
+impl Algorithm for BubbleSort {
+    fn render(&self, ctx: &mut BTerm) {
+        for (i, n) in self.vector.iter().enumerate() {
+            for y in 0..*n {
+                if i == self.j || i == self.j + 1 {
+                    ctx.set(i, y, GREEN, BLACK, to_cp437('#'));
+                } else if i > VECTOR_SIZE as usize - self.i - 1 {
+                    ctx.set(i, y, YELLOW, BLACK, to_cp437('#'));
+                } else {
+                    ctx.set(i, y, LIGHT_BLUE, BLACK, to_cp437('#'));
+                }
+            }
+        }
+    }
+
+    fn sort(&mut self) {
+        if self.j < VECTOR_SIZE as usize - self.i - 1 {
+            if self.vector[self.j] > self.vector[self.j + 1] {
+                self.vector.swap(self.j, self.j + 1);
+            }
+            self.j += 1;
+        } else if self.i < VECTOR_SIZE as usize {
+            self.i += 1;
+            self.j = 0;
+        }
+    }
+
+    fn reset(&mut self) {
+        self.vector = create_random_vector();
+        self.i = 0;
+        self.j = 0;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,66 @@
 #![warn(clippy::all, clippy::pedantic)]
 use bracket_lib::prelude::*;
-use rand::prelude::*;
-use std::fmt;
+use std::collections::HashMap;
+mod algo;
+use crate::algo::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 80;
-const VECTOR_SIZE: u16 = 80;
-
-fn create_random_vector() -> Vec<u16> {
-    let mut vec: Vec<u16> = (1..=VECTOR_SIZE).collect();
-    vec.shuffle(&mut thread_rng());
-    vec
-}
-
-struct SelectionSort {
-    vector: Vec<u16>,
-    i: usize,
-    j: usize,
-    smallest: usize,
-}
-
-impl SelectionSort {
-    fn new() -> Self {
-        SelectionSort {
-            vector: create_random_vector(),
-            i: 0,
-            j: 0,
-            smallest: 0,
-        }
-    }
-
-    fn render(&self, ctx: &mut BTerm) {
-        for (i, n) in self.vector.iter().enumerate() {
-            for y in 0..*n {
-                if i == self.smallest {
-                    ctx.set(i, y, RED, BLACK, to_cp437('#'));
-                } else if i == self.j {
-                    ctx.set(i, y, GREEN, BLACK, to_cp437('#'));
-                } else if i < self.i {
-                    ctx.set(i, y, YELLOW, BLACK, to_cp437('#'));
-                } else {
-                    ctx.set(i, y, LIGHT_BLUE, BLACK, to_cp437('#'));
-                }
-            }
-        }
-    }
-
-    fn sort(&mut self) {
-        if self.j < VECTOR_SIZE as usize {
-            if self.vector[self.j] < self.vector[self.smallest] {
-                self.smallest = self.j;
-            }
-            self.j += 1;
-        } else if self.i < VECTOR_SIZE as usize {
-            self.vector.swap(self.i, self.smallest);
-            self.i += 1;
-            self.j = self.i;
-            self.smallest = self.i;
-        }
-    }
-}
-
-struct BubbleSort {
-    vector: Vec<u16>,
-    i: usize,
-    j: usize,
-    smallest: usize,
-}
-
-impl BubbleSort {
-    fn new() -> Self {
-        BubbleSort {
-            vector: create_random_vector(),
-            i: 0,
-            j: 0,
-            smallest: 0,
-        }
-    }
-
-    fn render(&self, ctx: &mut BTerm) {
-        for (i, n) in self.vector.iter().enumerate() {
-            for y in 0..*n {
-                if i == self.smallest {
-                    ctx.set(i, y, RED, BLACK, to_cp437('#'));
-                } else if i == self.i {
-                    ctx.set(i, y, GREEN, BLACK, to_cp437('#'));
-                } else {
-                    ctx.set(i, y, YELLOW, BLACK, to_cp437('#'));
-                }
-            }
-        }
-    }
-    fn sort(&mut self) {
-        let range = self.vector.len();
-        for j in 0..range - self.i - 1 {
-            if self.vector[j] > self.vector[j + 1] {
-                self.vector.swap(j, j + 1);
-            }
-        }
-        self.i += 1;
-    }
-}
+const FRAME_DURATION: f32 = 75.0;
 
 // stores current state
 struct State {
-    algorithm: SelectionSort,
+    frame_time: f32,
+    current_key: String,
+    algorithms: HashMap<String, Box<dyn Algorithm>>,
 }
 
 impl State {
     fn new() -> Self {
+        // creating two vectors and combining them seems to work better
+        let keys = vec!["selection".into(), "bubble".into()];
+        let algos: Vec<Box<dyn Algorithm>> =
+            vec![Box::new(SelectionSort::new()), Box::new(BubbleSort::new())];
+
         State {
-            algorithm: SelectionSort::new(),
+            frame_time: 0.0,
+            current_key: "selection".into(),
+            algorithms: keys.into_iter().zip(algos.into_iter()).collect(),
         }
     }
 
+    // gets called on every game tick
     fn play(&mut self, ctx: &mut BTerm) {
+        // checks for user key presses
         match ctx.key {
-            Some(VirtualKeyCode::Key1) => println!("pressed 1"),
-            Some(VirtualKeyCode::Key2) => self.algorithm = BubbleSort::new(),
+            Some(VirtualKeyCode::Key1) => self.current_key = "selection".into(),
+            Some(VirtualKeyCode::Key2) => self.current_key = "bubble".into(),
 
             Some(VirtualKeyCode::Space) => {
-                self.algorithm.sort();
-                self.algorithm.render(ctx);
+                // add the ability to change the loop range
+                for _i in 0..10 {
+                    self.algorithms.get_mut(&self.current_key).unwrap().sort();
+                }
             }
-            Some(VirtualKeyCode::R) => self.restart(ctx),
+            Some(VirtualKeyCode::R) => self.restart(),
 
             Some(_) => println!("Unbinded"),
             None => (),
         }
+
+        // limits the amount of times the screen is being rendered
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+
+            ctx.cls_bg(BLACK);
+            self.algorithms[&self.current_key].render(ctx);
+            ctx.print(2, SCREEN_HEIGHT - 2, &self.current_key);
+        }
     }
 
-    fn restart(&mut self, ctx: &mut BTerm) {
-        self.algorithm.render(ctx);
+    fn restart(&mut self) {
+        self.algorithms.get_mut(&self.current_key).unwrap().reset();
     }
 }
 
